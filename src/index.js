da8b79379a86ff3ffbf730f320b6c717c88f6805
DESKTOP-S98238J\Raquel <Raquel Rodriguez Gonzalez>
2024-02-15 20:17:46 +0100

    implementación de logs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+const morgan = require('morgan') // Carga el logger de Morgan
+const bodyParser = require("body-parser");
+
 const app = express();
 const routes = require("./routes/index");
+
+// Config swagger
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerConfig = require("./swagger.json");
-
+const swaggerConfig = require("./config/swagger.json");
 const swaggerDocs = swaggerJsDoc(swaggerConfig);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use("/", routes);
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app
+  .use(cors())
+  .use(morgan('dev')) // Carga el middleware de Morgan
+  .use(bodyParser.json()) // to support JSON-encoded bodies
+  .use(
+    bodyParser.urlencoded({
+      // to support URL-encoded bodies
+      extended: true,
+    })
+  )
+  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs)) // ruta swagger
+  .use("/", routes); // enrutamientos
+
+// inicialización
+app.listen(process.env.port | 3000, () => {
+  console.log("Example app listening on port 3000!");
+});
