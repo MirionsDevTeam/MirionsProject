6cc5d3230e511a5537ce7e7c6b14b1a2fb7ecfe7
DESKTOP-S98238J\Raquel <Raquel Rodriguez Gonzalez>
2024-02-26 23:44:06 +0100

    añadido swagger en rutas de get de event y user

--- src/routes/api/v1/event.routes.js
+++ src/routes/api/v1/event.routes.js
@@ -2,8 +2,95 @@ const express = require("express");
 const eventController = require("../../../controllers/eventController");
 const router = express.Router();
 
+/**
+* @swagger
+* components:
+*  schemas:
+*   Event:
+*    type: object
+*    properties:
+*       title:
+*        type: string
+*       description:
+*        type: string
+*       provincia:
+*        type: string
+*       city:
+*        type: string
+*       address:
+*        type: string
+*       location:
+*        type: string
+*       eventType:
+*        type: string
+*       minGuests:
+*        type: integer
+*        format: int32
+*       maxGuests:
+*        type: integer
+*        format: int32
+*       observations:
+*        type: string
+*       createdAt:
+*        type: string
+*       createdBy:
+*        type: integer
+*        format: int32
+*       updatedAt:
+*        type: string
+*        format: date-time
+*       updatedBy:
+*        type: integer
+*        format: int32
+*       deletedAt:
+*        type: string
+*        format: date-time   
+*       deletedBy:
+*        type: integer
+*        format: int32
+* @swagger
+*  tags:
+*    name: Events
+*    description: Controlador de Eventos.
+ * @swagger
+ * /api/v1/events:
+ *  get:
+ *    tags: [Events]
+ *    description: Use to request all Events
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ *        content:
+ *          application/json:
+ *              schema:
+ *                  type: array
+ *                  items:
+ *                      $ref: '#/components/schemas/Event'
+ */
 router.get("/", eventController.getAllEvents);
 
+/**
+* @swagger
+* /api/v1/events/{id}:
+*  get:
+*    tags: [Events]
+*    description: Use to request one Event
+*    parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*         description: Numeric ID of the comando to get
+*    responses:
+*      '200':
+*        description: A successful response
+*        content:
+*          application/json:
+*              schema:
+*                  type: object
+*                  $ref: '#/components/schemas/Event'
+*/
 router.get('/:id',eventController.getEvent);
 
 module.exports = router;
--- src/routes/api/v1/user.routes.js
+++ src/routes/api/v1/user.routes.js
@@ -1,9 +1,98 @@
 const express = require("express");
 const userController = require("../../../controllers/userController");
 const router = express.Router();
-
+/**
+* @swagger
+* components:
+*  schemas:
+*   User:
+*    type: object
+*    properties:
+*       name:
+*        type: string
+*       surname:
+*        type: string
+*       email:
+*        type: string
+*       nickname:
+*        type: string
+*       password:
+*        type: string
+*       birthDate:
+*        type: string
+*        format: date-time
+*       imageUrl:
+*        type: string
+*       phone:
+*        type: string
+*       provincia:
+*        type: string
+*       city:
+*        type: string
+*       observations:
+*        type: string
+*       zipCode:
+*        type: string
+*       createdAt:
+*        type: string
+*       createdBy:
+*        type: integer
+*        format: int32
+*       updatedAt:
+*        type: string
+*        format: date-time
+*       updatedBy:
+*        type: integer
+*        format: int32
+*       deletedAt:
+*        type: string
+*        format: date-time   
+*       deletedBy:
+*        type: integer
+*        format: int32
+* @swagger
+*  tags:
+*    name: Users
+*    description: Controlador de Usuarios.
+ * @swagger
+ * /api/v1/users:
+ *  get:
+ *    tags: [Users]
+ *    description: Use to request all users
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ *        content:
+ *          application/json:
+ *              schema:
+ *                  type: array
+ *                  items:
+ *                      $ref: '#/components/schemas/User'
+ */
 router.get("/", userController.getAllUsers);
 
+/**
+* @swagger
+* /api/v1/users/{id}:
+*  get:
+*    tags: [Users]
+*    description: Use to request one comando
+*    parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*         description: Numeric ID of the user to get
+*    responses:
+*      '200':
+*        description: A successful response
+*        content:
+*          application/json:
+*              schema:
+*                  type: object
+*                  $ref: '#/components/schemas/User'
+*/
 router.get('/:id',userController.getUser);
 
 module.exports = router;
